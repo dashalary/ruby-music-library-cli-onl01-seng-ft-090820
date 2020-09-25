d904ca08560fe5b4a16534b08f6a066bf01d28dd
Dasha Lary <dk2800@nyu.edu>
2020-09-25 18:43:39 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -61,7 +61,7 @@ end
   end
  
  def list_songs_by_genre
-   puts 
+   puts "Please enter the name of a genre:"
    input = gets.strip 
    if genre = Genre.find_by_name(input)
     genre.songs.sort {|a, b| a.name <=> b.name}.each_with_index do |song, i|
